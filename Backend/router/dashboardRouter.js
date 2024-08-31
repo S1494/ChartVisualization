fe8e14f02c826f6648f072ae1d583501c34744f2
shubham kumawat <shubhamkumawat1494@yahoo.com>
2024-08-31 08:46:36 +0530

    jwt removed

--- Backend/router/dashboardRouter.js
+++ Backend/router/dashboardRouter.js
@@ -4,6 +4,6 @@ import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const dashboardRouter = Router();
 
-dashboardRouter.route("/").get(isAuthenticated, dashboardGet);
+dashboardRouter.route("/").get(dashboardGet); // remove the isAuthenticated as the tokens cause issue
 
 export default dashboardRouter;
